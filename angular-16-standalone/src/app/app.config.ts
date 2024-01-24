c935001fbb5913429200a5060dd7c7df42b5fa88
Ramesh Yadav <4143650+ram3shyadav@users.noreply.github.com>
2024-01-24 17:19:35 +0000

    ngrx added

--- angular-16-standalone/src/app/app.config.ts
+++ angular-16-standalone/src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { provideEffects } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore(reducers, { metaReducers }), provideEffects(AppEffects), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
--- angular-16/src/app/app.module.ts
+++ angular-16/src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
